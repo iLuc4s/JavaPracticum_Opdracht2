7c6ee545818ca901127c546a5f7fe1b0bc28e6d0
Robin Bosman <robin.bosman@gmail.com>
2015-11-28 21:02:12 +0100

    new

--- Opdracht 2/src/applicatie/Launcher.java
+++ Opdracht 2/src/applicatie/Launcher.java
@@ -1,28 +1,25 @@
 package applicatie;
 
-import domain.*;
-import domain.tellers.*;
-import java.util.ArrayList;
 import javax.swing.JFrame;
-import controller.SlotController;
+
+import controller.InstellingController;
 
 public class Launcher{
 
-   public static void main( String[] args ){
-      JFrame bedieningVenster = new JFrame( "Knack die code" );
-      
-      Teller t1= new CyclischeTeller('a','b','c');
-      Teller t2=new TerugLoopTeller();
-      Teller t3=new CyclischeTeller('*','+','@');  
-      ArrayList<Teller>tellers =new ArrayList<Teller>();
-      tellers.add(t1); tellers.add(t2); tellers.add(t3);
-      String geheimeCode = "a4+";
-      Slot slot =new Slot(tellers,geheimeCode);
-      
-      new SlotController(slot,bedieningVenster);
 
-      bedieningVenster.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-      bedieningVenster.setSize( 600, 400 ); 
-      bedieningVenster.setVisible( true ); 
+
+public static void main( String[] args ){
+	   
+	  
+	  JFrame instellingVenster = new JFrame ("Instellingen");
+	  
+	  new InstellingController(instellingVenster);
+	  
+	  instellingVenster.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      instellingVenster.setSize( 650, 700 ); 
+      instellingVenster.setVisible( true ); 
+
+	
+
    } 
 } 
